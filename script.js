dc76340d10d00555f3fdc363f15ea48faf335d30
Sveta <example@gmail.com>
2023-11-07 14:47:55 +0200

    Lest start creating the application

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-// const user = "Sveta";
-// alert (`Приіт,${user}`);
-const category = "Sveta";
-console.log (`https://www.facebook.com/${category}`);
+const numberOfFilms = +prompt('Сколько фильмов ви уже посмотрели?','');
+const personalMovieDB = {
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+};
+const a = prompt('Один из последних просимотрених фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просимотрених фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+      personalMovieDB.movies [a] = b;
+      personalMovieDB.movies [c] = d;
+      
+      console.log(personalMovieDB);
